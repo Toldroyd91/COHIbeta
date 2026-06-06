60a42eb972777d51dc8f43fd77e00853d999356c
Toldroyd91 <thomas.oldroyd@hotmail.com>
2026-06-06 19:20:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -753,4 +753,3 @@ document.addEventListener('DOMContentLoaded', function() {
         await executeSecurePDFGeneration('pdfTemplateCustomer', `${surname}_Design_Consultation.pdf`, this, data);
     });
 });
-
