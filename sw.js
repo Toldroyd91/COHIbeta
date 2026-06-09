760786f6e436c4d57b5e32dc20da47523dc40966
Toldroyd91 <thomas.oldroyd@hotmail.com>
2026-06-09 22:18:15 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,10 @@
-const CACHE_NAME = 'survey-app-v5-luxury';
+const CACHE_NAME = 'cohi-survey-v1';
 const ASSETS = [
-    './',
-    './index.html',
-    './style.css',
-    './app.js',
-    './designers.js',
-    './logo.jpg'
+    '/', '/index.html', '/style.css', '/app.js', '/designers.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/fabric.js/5.3.1/fabric.min.js'
 ];
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-    );
-});
-
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => response || fetch(e.request))
-    );
-});
+self.addEventListener('install', (e) => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', (e) => e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))));
